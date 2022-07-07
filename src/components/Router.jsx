3a5165c2d5e58a60870999f14bc4f51ee8b86918
areumsheep <areumsheep@gmail.com>
2022-07-07 15:51:14 +0900

    ✨ feat: 라우터 추가

---
+++ src/components/Router.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { SearchPage, BookmarkPage } from '@/pages';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SearchPage />} />
+      <Route path="/bookmark" element={<BookmarkPage />} />
+    </Routes>
+  );
+};
+
+export default Router;
