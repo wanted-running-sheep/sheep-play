5225aad7b4ba0d682a5684cf143903844e35825b
hajun2 <jpd93@naver.com>
2022-07-08 15:59:27 +0900

    ✨ feat: 슬라이드 기능 구현, 버튼 추가

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import Slide from '@/components/Slide';
+import axios from 'axios';
+
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    axios.get('http://localhost:3001/movies') //자체적으로 데이터 테스트용도로 3001포트 사용
+      .then((res) => {
+        setMovies(res.data.slice(0,10));  
+      });
+  }, []);
+  return (
+    <div>
+      {/* !!배열을 분산해서 10개씩 슬라이드에 넣어서 매핑해줘야 함. */}
+      <Slide movies={movies}/>
+    </div>
+  )
 };
 
 export default SearchPage;
