2c84b75371e675c5fa59e28de9bc123998e3d22c
dlwjdals <jml22zzang@naver.com>
2022-07-08 18:17:39 +0900

    ✨ feat: 무한스크롤 기능 구현

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import InfiniteScroll from '../components/InfiniteScroll';
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div>
+      SearchPage
+      <InfiniteScroll></InfiniteScroll>
+    </div>
+  );
 };
 
 export default SearchPage;
